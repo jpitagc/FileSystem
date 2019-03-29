1f63cc58f576b7a13fe7f885847f055c27687441
jpitagc <jpitagc@gmail.com>
2019-03-29 15:36:34 +0100

    Initial Code

---
+++ filesystem.c
@@ -0,0 +1,128 @@
+/*
+ * OPERATING SYSTEMS DESING - 16/17
+ *
+ * @file 	filesystem.c
+ * @brief 	Implementation of the core file system funcionalities and auxiliary functions.
+ * @date	01/03/2017
+ */
+
+#include "include/filesystem.h" // Headers for the core functionality
+#include "include/auxiliary.h"  // Headers for auxiliary functions
+#include "include/metadata.h"   // Type and structure declaration of the file system
+
+/*
+ * @brief 	Generates the proper file system structure in a storage device, as designed by the student.
+ * @return 	0 if success, -1 otherwise.
+ */
+int mkFS(long deviceSize)
+{
+	return -1;
+}
+
+/*
+ * @brief 	Mounts a file system in the simulated device.
+ * @return 	0 if success, -1 otherwise.
+ */
+int mountFS(void)
+{
+	return -1;
+}
+
+/*
+ * @brief 	Unmounts the file system from the simulated device.
+ * @return 	0 if success, -1 otherwise.
+ */
+int unmountFS(void)
+{
+	return -1;
+}
+
+/*
+ * @brief	Creates a new file, provided it it doesn't exist in the file system.
+ * @return	0 if success, -1 if the file already exists, -2 in case of error.
+ */
+int createFile(char *path)
+{
+	return -2;
+}
+
+/*
+ * @brief	Deletes a file, provided it exists in the file system.
+ * @return	0 if success, -1 if the file does not exist, -2 in case of error..
+ */
+int removeFile(char *path)
+{
+	return -2;
+}
+
+/*
+ * @brief	Opens an existing file.
+ * @return	The file descriptor if possible, -1 if file does not exist, -2 in case of error..
+ */
+int openFile(char *path)
+{
+	return -2;
+}
+
+/*
+ * @brief	Closes a file.
+ * @return	0 if success, -1 otherwise.
+ */
+int closeFile(int fileDescriptor)
+{
+	return -1;
+}
+
+/*
+ * @brief	Reads a number of bytes from a file and stores them in a buffer.
+ * @return	Number of bytes properly read, -1 in case of error.
+ */
+int readFile(int fileDescriptor, void *buffer, int numBytes)
+{
+	return -1;
+}
+
+/*
+ * @brief	Writes a number of bytes from a buffer and into a file.
+ * @return	Number of bytes properly written, -1 in case of error.
+ */
+int writeFile(int fileDescriptor, void *buffer, int numBytes)
+{
+	return -1;
+}
+
+/*
+ * @brief	Modifies the position of the seek pointer of a file.
+ * @return	0 if succes, -1 otherwise.
+ */
+int lseekFile(int fileDescriptor, long offset, int whence)
+{
+	return -1;
+}
+
+/*
+ * @brief	Creates a new directory provided it it doesn't exist in the file system.
+ * @return	0 if success, -1 if the directory already exists, -2 in case of error.
+ */
+int mkDir(char *path)
+{
+	return -2;
+}
+
+/*
+ * @brief	Deletes a directory, provided it exists in the file system.
+ * @return	0 if success, -1 if the directory does not exist, -2 in case of error..
+ */
+int rmDir(char *path)
+{
+	return -2;
+}
+
+/*
+ * @brief	Lists the content of a directory and stores the inodes and names in arrays.
+ * @return	The number of items in the directory, -1 if the directory does not exist, -2 in case of error..
+ */
+int lsDir(char *path, int inodesDir[10], char namesDir[10][33])
+{
+	return -2;
+}
